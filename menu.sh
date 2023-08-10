c14034283a98baa6234f6fbc612435c994899728
rocio <rociopq4@gmail.com>
2023-08-10 00:30:00 -0300

    script completado

--- menu.sh
+++ menu.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-#falta script
+# Función para la opción generar.sh
+generar() {
+    read -p "Ingrese la cantidad de imágenes a generar: " cantidad_imagenes
+    bash generar.sh "$cantidad_imagenes"
+}
+
+# Función para la opción descomprimir.sh
+descomprimir() {
+    read -p "Ingrese el nombre del archivo comprimido: " archivo_comprimido
+    read -p "Ingrese el nombre del archivo de suma de verificación: " archivo_s>
+    bash descomprimir.sh "$archivo_comprimido" "$archivo_suma_verificacion"
+}
+
+# Función para la opción procesar.sh
+procesar() {
+    bash procesar.sh
+}
+
 
